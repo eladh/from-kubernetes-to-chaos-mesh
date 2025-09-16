5c962d3b499f5b06d1c33917a0cc0810e42c5255
Elad Hirsch <hirsch.elad@gmail.com>
2025-09-16 13:03:48 -0400

    Update run-demo.sh
    fixed

--- demo/run-demo.sh
+++ demo/run-demo.sh
@@ -9,35 +9,51 @@ kubectl delete -f ../chaos-mesh/frontend_chaos.yaml
 
 clear
 
-pe "kubectl get nodes"
-
-pe "kubectl get pods -n microservices-demo"
-
-pe "kubectl get svc -n microservices-demo"
-
-pe "kubectl get ingress -n microservices-demo"
+ARGOCD_URL=$(kubectl get ingress argocd -n argocd -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+pe "open http://${ARGOCD_URL}"
 
 STORE_URL=$(kubectl get ingress frontend-ingress -n microservices-demo -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 CHAOS_MESH_URL=$(kubectl get ingress chaos-dashboard-ingress -n chaos-mesh -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+
+clear
 
 pe "open http://${STORE_URL}"
 
 pe "kubectl get pods -n pl"
 
-pe " # The vizier-pem* are data collectors, deployed to each node via a daemonset, responsible for the eBPF-based monitoring and telemetry collection "
+echo ""
+echo "############################ ℹ️ ################################"
+echo "#                                                             #"
+echo "#  The vizier-pem* are data collectors, deployed to each node #"
+echo "#  via a DaemonSet, responsible for the eBPF-based monitoring #"
+echo "#  and telemetry collection                                   #"
+echo "#                                                             #"
+echo "###############################################################"
+echo ""
 
-# invite link - https://getcosmic.ai/invite?invite_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTc5NzQxODksInN1YiI6ImU0NWI2NmM2LWVhZmQtNGEwYi1iZTQ4LWFjYWFiMGJjODgzOCJ9.MQ46zU24DwgRn4XLNv_HpKL-95G-unWL4H5BeMhcAgg
 pe "open https://work.getcosmic.ai/live/clusters"
+
+clear
 
 pe "kubectl get pods -n chaos-mesh"
 
-pe " # The chaos-daemon* runs as a DaemonSet with Privileged permissions to interfere with network, filesystem, and kernel in target Pod Namespaces."
+echo ""
+echo "########################### ℹ️ ################################"
+echo "#                                                             #"
+echo "#  The chaos-daemon* runs as a DaemonSet with Privileged      #"
+echo "#  permissions to interfere with network, filesystem, and     #"
+echo "#  kernel in target Pod Namespaces.                           #"
+echo "#                                                             #"
+echo "###############################################################"
+echo ""
 
 pe "kubectl create token chaos-dashboard-user --duration=1h"
 
 pe "open http://${CHAOS_MESH_URL}"
 
-#baeline
+clear
+
+#baseline
 pe "ab -d -n 50 -c 10 http://${STORE_URL}/"
 
 # first chaos experiment
@@ -46,18 +62,20 @@ pe "cat ../chaos-mesh/productcatalogservice_chaos.yaml"
 pe "kubectl apply -f ../chaos-mesh/productcatalogservice_chaos.yaml"
 
 pe "ab -d -n 50 -c 10 http://${STORE_URL}/"
+
 # remove chaos experiment
 pe "kubectl delete -f ../chaos-mesh/productcatalogservice_chaos.yaml"
 
+clear
+
 pe "cat ../chaos-mesh/frontend_chaos.yaml"
+
 #adding chaos experiment
 pe "kubectl apply -f ../chaos-mesh/frontend_chaos.yaml"
 
 pe "ab -d -n 50 -c 10 http://${STORE_URL}/"
 
-pe "open http://${STORE_URL} && clear"
-
-pe "# ⚠️ Very strange behavior detected! ⚠️ , Why 250ms delay = almost 3.5s+ latency? 🔍"
+pe "# ⚠️ Very strange behavior detected! ⚠️ , Why 250ms delay = almost 4s latency? 🔍"
 
 # So how pixie help , its not what its showing in the ui , it's the easy way its helpping you to get without is the tool that will help us understand the latency issue
 "./pixie-latency-analysis.sh"
